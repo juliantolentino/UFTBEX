fcd84b5b4933e3c8446453cadf95a01223a55bd6
juliantolentino <julianntolentino@gmail.com>
2017-11-15 18:12:06 -0500

    changed titles and support content

--- config/env/default.js
+++ config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   app: {
-    title: 'issle',
+    title: 'Textbooks@UF',
     description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
     keywords: 'MongoDB, Express, AngularJS, Node.js',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
